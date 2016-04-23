edcef9cc9bc2de54b352873e786937738046463f
tlwlmy <tlwlmy@github.com>
2016-04-23 20:09:00 +0800

    修改配置

--- manage.py
+++ manage.py
@@ -5,28 +5,16 @@
 # Copyright © 2015 xym
 #
 from app import create_app
-#from app.common.functions import api_response
+from app.common.functions import api_response
 from flask.ext.script import Manager
 
-import json
-from flask import request, jsonify
-
-def api_response(contents, code=200):
-    """返回API的响应
-    :param contents: dict, JSON内容
-    :param code: int, HTTP CODE
-    :return: response
-    """
-    response = jsonify(contents)
-    response.status_code = code
-    return response
 app = create_app()
 manager = Manager(app)
 
 @app.route('/')
 def index():
     return api_response({
-        'adapi-cn': 'https://git.umlife.net/adsys/adapi-cn',
+        'flask_base': 'https://github.com/tlwlmy/flask_base',
     })
 
 if __name__ == '__main__':
