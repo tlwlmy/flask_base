dfce49f157a48b2a98e8f762663bf64110e1e4c5
tlwlmy <tlwlmy@github.com>
2016-04-26 11:10:21 +0800

    添加缓存时间间隔

--- app/common/constant.py
+++ app/common/constant.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# 缓存的有效时间
+class Duration:
+    HalfMin   = 30;
+    OneMin    = 60;
+    TwoMin    = 120;
+    ThrMin    = 180;
+    FiveMin   = 300;
+    TenMin    = 600;
+    FifMin    = 900;
+    TwentyMin = 1200;
+    HalfHour  = 1800;
+    OneHour   = 3600;
+    TwoHour   = 7200;
+    FiveHour  = 18000;
+    HalfDay   = 43200;
+    OneDay    = 86400;
+    TenDay    = 864000;
+
